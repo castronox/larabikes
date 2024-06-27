1469788c864567269bed428c4344a2f0893172fe
Cristian Castro <41049193+castronox@users.noreply.github.com>
2024-06-27 13:02:07 +0200

    Routing 1 Aplicados el csrf ,ACORDARSE DE HABILITARLO MAS ADELANTE!!!

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 use App\Http\Controllers\BikeController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+#CRUD DE MOTOS
+Route::resource('bikes', BikeController::class);
 
+Route::get('/',[WelcomeController::class, 'index'])->name('portada');
 
+#Ruta de FALLBACK
+Route::fallback([WelcomeController::class, 'index']);
+
+# Formulario de confirmación de eliminación
 Route::get('bikes/{bike}/delete', [BikeController::class, 'delete'])
     ->name('bikes.delete');             // Eliminar moto.php 
 
 
-Route::resource('bikes', BikeController::class);
+
+
+
+
+
 
