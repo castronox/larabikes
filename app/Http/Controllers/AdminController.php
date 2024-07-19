59932d4b57d53422759497116d25075078e70530
Castronox <castronox@outlook.es>
2024-07-19 11:10:32 +0200

    Añadido la vista de la lista de motos desde la zona de mostrar usuarios del administrador

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,55 +1,61 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Bike;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+
+
     # Muestra la lista de usuarios
-    public function userList(){
-        $users =User::orderBy('name', 'ASC')
+    public function userList()
+    {
+        $users = User::orderBy('name', 'ASC')
             ->paginate(config('pagination.users', 10));
-            return view('admin.users.list', ['users' =>$users]);
+        return view('admin.users.list', ['users' => $users]);
     }
 
     # Muestra un usuario
-    public function userShow(User $user){
-            
-        # Carga la vista de detalles y le pasa el usuario recuperado
-        return view('admin.users.show', ['user' => $user]);
+    public function userShow(User $user)
+    {
+        $bikes = $user->bikes; // Asumiendo que tienes una relación definida entre User y Bikes
+        return view('admin.users.show', ['user' => $user, 'bikes' => $bikes]);
     }
 
     # Método para buscar usuarios 
-    public function userSearch(Request $request){
+    public function userSearch(Request $request)
+    {
         $request->validate(['name' => 'max:32', 'email' => 'max:32']);
 
         # Toma todos los valores que llegan para nombre y email
         $name = $request->input('name', '');
-        $email = $request->input('email','');
+        $email = $request->input('email', '');
 
         # Recupera los resultados, añadimos marca y modelo al paginador
         # para que haga bien los enlaces y se mantenga el filtro al pasar de página
         $users = User::orderBy('name', 'ASC')
-            ->where('name','like',"%$name%")
+            ->where('name', 'like', "%$name%")
             ->where('email', 'like', "%$name%")
             ->paginate(config('pagination.users'))
             ->appends(['name' => $name, 'email' => $email]);
 
         # Retorna la vista del filtro aplicado
-        return view('admin.users.list', ['users'=>$users, 'name'=>$name, 'email'=>$email]);        
-    } 
+        return view('admin.users.list', ['users' => $users, 'name' => $name, 'email' => $email]);
+    }
 
 
 
 
 
-    public function deletedBikes(){
+    public function deletedBikes()
+    {
 
         # Recupera las motos
         $bikes = Bike::onlyTrashed()
-                    ->paginate(config('pagination.bikes', 10));
+            ->paginate(config('pagination.bikes', 10));
 
         # Carga la vista
         return view('admin.bikes.deleted', ['bikes' => $bikes]);
--- public/resources/views/admin/users/show.blade.php
+++ public/resources/views/admin/users/show.blade.php
@@ -35,13 +35,68 @@
 
         <figure class="col-4">
             <img class="rounded img-fluid" alt="Imagen de usuario {{ $user->name }}"
-                src="{{ asset('storage/images/users/default.jpg') }}">
+                src="{{ asset('storage/images/users/default.png') }}">
             <figcaption class="figure-caption text-center">
                 {{ $user->name }}
             </figcaption>
         </figure>
     </div>
+
+    <h2>Mis motos</h2>
+    <div class="container">
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Foto</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Matricula</th>
+                    <th>Color</th>
+                    <th>Operaciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($bikes as $bike)
+                <tr>
+                    <td>{{ $bike->id }}</td>
+                    <td class="text-start d-flex justify-content-center">
+                        <img class="rounded" style="max-width: 90px;"
+                            src="{{ $bike->imagen ? asset('storage/' . config('filesystems.bikesImageDir')) . '/' . $bike->imagen : asset('storage/' . config('filesystems.bikesImageDir')) . '/default.png' }}"
+                            alt="Imagen de {{ $bike->marca }} {{ $bike->modelo }}"
+                            title="Imagen de {{ $bike->marca }} {{ $bike->modelo }}">
+                    </td>
+                    <td>{{ $bike->marca }}</td>
+                    <td>{{ $bike->modelo }}</td>
+                    <td>{{ $bike->matricula }}</td>
+                    <td style="background-color: {{ $bike->color }}">{{ $bike->color }}</td>
+                    <td class="text-center">
+                        <a href="{{ route('bikes.show', $bike->id) }}" class="btn btn-info btn-sm">
+                            <img height="20" width="20" src="{{ asset('images/buttons/show.png') }}" alt="Ver detalles"
+                                title="Ver detalles">
+                        </a>
+                        @auth
+                        @if (Auth::user()->can('update', $bike))
+                        <a href="{{ route('bikes.edit', $bike->id) }}" class="btn btn-warning btn-sm">
+                            <img height="20" width="20" src="{{ asset('images/buttons/update.png') }}" alt="Modificar"
+                                title="Modificar">
+                        </a>
+                        @endif
+                        @can('update', $bike)
+                        <a href="{{ route('bikes.delete', $bike->id) }}" class="btn btn-danger btn-sm">
+                            <img height="20" width="20" src="{{ asset('images/buttons/delete.png') }}" alt="Eliminar"
+                                title="Eliminar">
+                        </a>
+                        @endcan
+                        @endauth
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
+
 @section('enlaces')
     @parent
     <a href="{{ route('admin.users') }}" class="btn btn-primary m-2">Lista de usuarios</a>
